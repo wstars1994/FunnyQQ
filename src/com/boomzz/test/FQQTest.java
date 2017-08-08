c55dcec7e1ce11afc8b2f6e1183404ff61b2ae68
wxc <545640807@qq.com>
2017-08-08 16:34:13 +0800

    login

--- src/com/boomzz/test/FQQTest.java
+++ src/com/boomzz/test/FQQTest.java
@@ -10,7 +10,9 @@ public class FQQTest {
 		//获取实例
 		AbstractLogin qrLogin = new QRCodeLogin(new QQListener());
 		//登录并返回Message实例  此类可以获得好友群讨论组信息以及发送消息
-		Message msg = qrLogin.login(2);
+
+		int loginType = 1; //登录方式 1:重新扫码登录;2:缓存登录(可能会超时失效)
+		Message msg = qrLogin.login(loginType);
 		if(msg==null){
 			System.out.println("登录失败,请查看登录日志");
 		}else {
